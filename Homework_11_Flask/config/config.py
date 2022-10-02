e4d9876171e6758ecaaf36b4f0d3d2d7fa046a10
Olena Boldysheva <adv55577@gmail.com>
2022-10-02 21:52:18 +0300

    update config

--- Homework_11_Flask/config/config.py
+++ Homework_11_Flask/config/config.py
@@ -1,9 +1,12 @@
 import pathlib
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 BASE_DIR = pathlib.Path(__file__).parent.parent
 
 
 class Config:
-    SECRET_KEY = b'g\xc0\x7f\x02[U\x9dJ\x10B\xae\x1cGK\x1b\xcb'
+    SECRET_KEY = config['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / 'data' / 'contact.sql')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
